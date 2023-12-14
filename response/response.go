6efce3f1d8ef5718cdf2d842c13ef424cfb0fd59
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2023-12-13 22:54:09 -0300

    feat: add support for echo framework in responses

--- response/response.go
+++ response/response.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/labstack/echo/v4"
 	"github.com/valyala/fasthttp"
 	"google.golang.org/grpc/status"
 )
@@ -24,95 +25,83 @@ type Responser interface {
 }
 
 type Response struct {
+	customCode  int
 	serviceName string
 	contentType string
-	ctx         context.Context
-	errors      *ErrorOptions
+	ctx         interface{}
 }
 
 type Options struct {
 	ServiceName string
-	Errors      *ErrorOptions
 }
 
-type ErrorOptions struct {
-	ShowInternalMessages bool
-	ShowSource           bool
-}
-
-// New creates a new response container for HTTP handlers return data using a
+// NewFromFasthttp creates a new response container for HTTP handlers return data using a
 // specific standard.
-func New(ctx context.Context, options *Options) *Response {
-	var contentType string
-
-	if fctx, ok := ctx.(*fasthttp.RequestCtx); ok {
-		contentType = string(fctx.Request.Header.Peek(contentTypeHeader))
-	}
-
+func NewFromFasthttp(ctx *fasthttp.RequestCtx, options *Options) *Response {
 	return &Response{
 		serviceName: options.ServiceName,
-		contentType: contentType,
+		contentType: string(ctx.Request.Header.Peek(contentTypeHeader)),
 		ctx:         ctx,
-		errors:      options.Errors,
 	}
 }
 
-func (r *Response) ForwardAuthenticationError(err error) {
+func NewFromEcho(ctx echo.Context, options *Options) *Response {
+	return &Response{
+		serviceName: options.ServiceName,
+		contentType: "application/json",
+		ctx:         ctx,
+	}
+}
+
+func (r *Response) ForwardAuthenticationError(err error) error {
 	ferror, err := serviceErrorFromString(err.Error())
 	if err != nil {
-		r.forwardOutput(fasthttp.StatusInternalServerError,
+		return r.forwardOutput(fasthttp.StatusInternalServerError,
 			newResponseError(&responseErrorOptions{
 				Message: internalServerErrorMsg,
 				Details: err.Error(),
 			}),
 		)
-
-		return
 	}
 	if ferror.IsKnownError() {
-		r.forwardOutput(ferror.ResponseCode(), ferror.ToResponseError())
-		return
+		return r.forwardOutput(ferror.ResponseCode(), ferror.ToResponseError())
 	}
+
+	return nil
 }
 
-func (r *Response) ForwardError(err error) {
+func (r *Response) ForwardError(err error) error {
 	ferror, err := serviceErrorFromString(err.Error())
 	if err != nil {
-		r.forwardOutput(fasthttp.StatusInternalServerError,
+		return r.forwardOutput(fasthttp.StatusInternalServerError,
 			newResponseError(&responseErrorOptions{
 				Message: internalServerErrorMsg,
 				Details: err.Error(),
 			}),
 		)
-
-		return
 	}
 	if ferror.IsKnownError() {
-		r.forwardOutput(ferror.ResponseCode(), ferror.ToResponseError())
-		return
+		return r.forwardOutput(ferror.ResponseCode(), ferror.ToResponseError())
 	}
 
 	// A gRPC service can send "gRPC" errors in case of unexpected errors
 	if sts, ok := status.FromError(err); ok {
-		r.forwardOutput(fasthttp.StatusInternalServerError,
+		return r.forwardOutput(fasthttp.StatusInternalServerError,
 			newResponseError(&responseErrorOptions{
 				Message: internalServerErrorMsg,
 				Details: sts.Message(),
 			}),
 		)
-
-		return
 	}
 
 	// In case some parsing failed.
 	if res, ok := jsonError(err); ok {
-		r.forwardOutput(fasthttp.StatusBadRequest, res)
-		return
+		return r.forwardOutput(fasthttp.StatusBadRequest, res)
 	}
 
 	// Forward the original error if none of the above error checks were
 	// successful.
-	r.forwardOutput(fasthttp.StatusInternalServerError,
+	return r.forwardOutput(fasthttp.StatusInternalServerError,
 		newResponseError(&responseErrorOptions{
 			Source:  r.serviceName,
 			Message: internalServerErrorMsg,
@@ -121,33 +110,23 @@ func (r *Response) ForwardError(err error) {
 	)
 }
 
-func (r *Response) ForwardSuccess(data interface{}) {
+func (r *Response) ForwardSuccess(data interface{}) error {
 	// Does the message have another format to send as response?
 	if h, ok := data.(Responser); ok {
 		data = h.HttpResponse()
 	}
 
-	r.forwardOutput(fasthttp.StatusOK, data)
+	return r.forwardOutput(fasthttp.StatusOK, data)
 }
 
-func (r *Response) setCustomHeaders(ctx *fasthttp.RequestCtx) {
-	// Set all handler's custom header values.
-	ctx.VisitUserValues(func(key []byte, value interface{}) {
-		if strings.HasPrefix(string(key), customHeaderPrefix) {
-			ctx.Response.Header.Set(strings.TrimPrefix(string(key), customHeaderPrefix), value.(string))
-		}
-	})
-}
-
-func (r *Response) forwardOutput(statusCode int, data interface{}) {
+func (r *Response) forwardOutput(statusCode int, data interface{}) error {
 	out, err := json.Marshal(data)
 	if err != nil {
-		r.ForwardError(err)
-		return
+		return r.ForwardError(err)
 	}
 
 	if fctx, ok := r.ctx.(*fasthttp.RequestCtx); ok {
-		r.setCustomHeaders(fctx)
+		r.setFasthttpCustomHeaders(fctx)
 
 		if v := fctx.UserValue(customResponseCode); v != nil {
 			if c, ok := v.(int); ok {
@@ -158,10 +137,53 @@ func (r *Response) forwardOutput(statusCode int, data interface{}) {
 		fctx.Response.SetStatusCode(statusCode)
 		fctx.Response.Header.SetContentType(r.contentType)
 		fctx.Response.SetBodyRaw(out)
-		return
+
+		return nil
 	}
+
+	if ectx, ok := r.ctx.(echo.Context); ok {
+		if r.customCode != 0 {
+			statusCode = r.customCode
+		}
+
+		ectx.Response().Header().Set("Content-Type", r.contentType)
+
+		if err := ectx.JSON(statusCode, out); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *Response) setFasthttpCustomHeaders(ctx *fasthttp.RequestCtx) {
+	// Set all handler's custom header values.
+	ctx.VisitUserValues(func(key []byte, value interface{}) {
+		if strings.HasPrefix(string(key), customHeaderPrefix) {
+			ctx.Response.Header.Set(strings.TrimPrefix(string(key), customHeaderPrefix), value.(string))
+		}
+	})
 }
 
 func (r *Response) SetContentType(contentType string) {
 	r.contentType = contentType
+}
+
+func SetResponseCode(ctx context.Context, code int) {
+	if c, ok := ctx.(*fasthttp.RequestCtx); ok {
+		c.SetUserValue(customResponseCode, code)
+		return
+	}
+
+	r := RetrieveFromContext(ctx)
+	r.customCode = code
+}
+
+func AppendResponseToContext(ctx context.Context, r *Response) context.Context {
+	return context.WithValue(ctx, "response", r)
+}
+
+func RetrieveFromContext(ctx context.Context) *Response {
+	r := ctx.Value("response")
+	return r.(*Response)
 }
